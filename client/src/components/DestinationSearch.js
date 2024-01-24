15699a598dd27d0e61d1ee86b1a344ce498201b3
tfkjosh <jkg050604@gmail.com>
2024-01-23 21:28:12 -0600

    destination

--- client/src/components/DestinationSearch.js
+++ client/src/components/DestinationSearch.js
@@ -1,4 +1,21 @@
 import React, { useState } from 'react';
 
 // DestinationSearch component
-const DestinationSearch = () => {};
+const DestinationSearch = () => {
+    return (
+        <div>
+            <header>
+                <h1>Destination Search</h1>
+            </header>
+            <main className='container'>
+                <form className='border p-2' id= 'city-search-form'>
+                    <h2>Search for City</h2>
+                    <input id='city' type='text' autofocus='true' className='form-input'>
+                    <button className='btn btn-primary' type='submit'>Search</button>
+                    </input>
+                </form>
+            </main>
+        </div>
+    )
+};
+export default DestinationSearch;

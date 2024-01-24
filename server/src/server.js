e7b862783fcaac43711f60aed800b4fd3197b31b
Phuc Dinh <ams.dinhphuc@gmail.com>
2024-01-23 18:32:54 -0600

    Set up the environment variables to a MongoDB database and code for the server.js file!

---
+++ server/src/server.js
@@ -0,0 +1,34 @@
+// Import required packages
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+
+// Load environment varaibles from .env file
+require('dotenv').config();
+
+// Create an instance of Express
+const app = express();
+
+// Connect to MongoDB using environment variables 
+mongoose.connect(process.env.DB_CONNECTION, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log(error));
+
+// Create an instance of ApolloServer
+const server = new ApolloServer({
+    // TODO: Add typeDefs and resolvers here
+});
+
+// Apply the ApolloServer middleware to Express
+server.applyMiddleware({ app });
+
+// Set the port for the server to listen on 
+const port = process.env.DB_PORT || 4000; 
+
+// Start the server
+app.listen(port, () => {
+    console.log('Server running on port 4000'); 
+});

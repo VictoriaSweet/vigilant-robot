96391462373ac19c36c18a6b68dab568e6b47c59
VictoriaSweet <sweet.victoria1218@gmail.com>
2024-01-25 13:17:13 -0600

    fixed server and added intial API endopint

---
+++ client/src/services/api.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://booking-com15.p.rapidapi.com/api/v1/meta/getLanguages',
+  headers: {
+    'X-RapidAPI-Key': 'c740526878mshc6c3d746e2f3003p19d0f3jsn8f869006c8ff',
+    'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}

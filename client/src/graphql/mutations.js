314024b930b32f2e16e2ecbe781629b284c45283
tfkjosh <jkg050604@gmail.com>
2024-01-30 11:22:44 -0600

    mutations and queries

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -1,33 +1,88 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-
-export const GET_USER = gql`
-mutation getUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
-    ) {
-      token
-      user {
-        _id
-      }
-    }
-  }
-`;
+import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
+        username
       }
     }
+  }
+`;
+export const CREATE_USER = gql`
+  mutation {
+    createUser(id: ID!, $username: String!, $email: String!, $password: String!): User
+  }
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+  }
+`;
+export const CREATE_TRAVELPLAN = gql`
+  mutation {
+    createTravelPlan($user: ID!, $destination: ID!, $day: String!, $activities: [String], $accommodationReservation: String!, $activitiesReservation: [String]!, $travelArrangements: String!): TravelPlan
+  }
+  input TravelPlanInput {
+    user: ID!
+    destination: ID!
+    itinerary: [$day: String!, $activities: [String]]
+    accommodationReservation: String
+    activitiesReservation: [String]
+    travelArrangements: String
+  }
+  type TravelPlan {
+    _id: ID!
+    user: User!
+    destination: Destination!
+    itinerary: [ItineraryDay]  # Adjusted to be an array
+    accommodationReservation: String
+    activitiesReservation: [String]
+    travelArrangements: String
+  }
+`;
+export const CREATE_DESTINATION = gql`
+  mutation {
+    createDestination($name: String!, $description: String!, $attractions: [String]!, $accommodationOptions: [String]! ): Destination
+  }
+  input DestinationInput {
+    name: String!
+    description: String
+    attractions: [String]
+    accommodationOptions: [String]
+  }
+  type Destination {
+    _id: ID!
+    name: String!
+    description: String
+    attractions: [String]
+    accommodationOptions: [String]
+  }
+`;
+export const CREATE_QUOTE = gql`
+  mutation {
+    createQuote($destination: ID!, $flightCost: Float!, $accommodationCost: Float!, $activitiesCost: Float!, $totalCost: Float!): Quote
+  }
+  input QuoteInput {
+    destination: ID!
+    flightCost: Float!
+    accommodationCost: Float!
+    activitiesCost: Float!
+    totalCost: Float!
+  }
+  type Quote {
+    _id: ID!
+    destination: Destination!
+    flightCost: Float!
+    accommodationCost: Float!
+    activitiesCost: Float!
+    totalCost: Float!
   }
 `;
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,11 +1,71 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-const GET_USER = gql`
-  query GetUser($userId: ID!) {
-    getUser(userId: $userId) {
-      id
-      username
-      email
+import { gql } from '@apollo/client';
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id: ID!
+      username: String!
+      email: String!
+      password: String!
     }
+  }
+`;
+export const QUERY_DESTINATION = gql`
+  query {
+    getDestination(id: ID!): Destination
+  }
+  type Destination {
+    _id: ID!
+    name: String!
+    description: String
+    attractions: [String]
+    accommodationOptions: [String]
+  }
+`;
+export const QUERY_QUOTE = gql`
+  query {
+    getQuote(id: ID!): Quote
+  }
+  type Quote {
+    _id: ID!
+    destination: Destination!
+    flightCost: Float!
+    accommodationCost: Float!
+    activitiesCost: Float!
+    totalCost: Float!
+  }
+`;
+export const QUERY_TRAVELPLAN = gql`
+  query {
+    getTravelPlan(id: ID!): TravelPlan
+  }
+  type TravelPlan {
+    _id: ID!
+    user: User!
+    destination: Destination!
+    itinerary: [ItineraryDay]  # Adjusted to be an array
+    accommodationReservation: String
+    activitiesReservation: [String]
+    travelArrangements: String
+  }
+`;
+export const QUERY_ITINERARYDAY = gql`
+  query {
+    getItineraryDay(day: String!): Itinerary Day
+  }
+  type ItineraryDay {
+    day: String!
+    activities: [String]
+  }
+`;
+export const QUERY = gql`
+  query {
+    getQuery(name: String!): Destination
+  }
+   type Query {
+    destination(name: String!): Destination
+    destinations: [Destination]
+    quotes: [Quote]
+    travelPlans: [TravelPlan]
+    users: [User]
   }
 `;
